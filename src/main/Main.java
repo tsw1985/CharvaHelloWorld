7038127b50c69e6209ad26405a97dd96079ca2df
gabriel <gabriel@gabriel.com>
2017-10-15 11:46:30 +0100

    Separed files

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,29 +12,18 @@ import charvax.swing.JLabel;
 
 public class Main {
 
+	
+	
 	// API: http://www.pitman.co.za/projects/charva/api/allclasses-noframe.html
 	public static void main(String[] args) {
-
-		Toolkit toolKit = Toolkit.getDefaultToolkit();
 		
-		System.setProperty("charva.color", "");
-		JFrame fr = new JFrame();
-		fr.setTitle("Hello World");
-		fr.setBackground(Color.blue);
-		fr.setSize(toolKit.getScreenSize());
-		Container contentPane = fr.getContentPane();
-		contentPane.setLayout(new BorderLayout());
-		contentPane.add(new JLabel("Hello World"), BorderLayout.CENTER);
+		MainWindow mainWindow = new MainWindow();
+		mainWindow.show();
 
-		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-
-		contentPane.add(exitButton, BorderLayout.SOUTH);
-		fr.show();
+		
+		
+		
+		
 
 	}
 
--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -1,12 +1,50 @@
 package main;
 
+import charva.awt.BorderLayout;
+import charva.awt.Color;
 import charva.awt.Container;
+import charva.awt.Toolkit;
+import charva.awt.event.ActionEvent;
+import charva.awt.event.ActionListener;
+import charvax.swing.JButton;
+import charvax.swing.JFrame;
+import charvax.swing.JLabel;
 
-public class MainWindow extends Container{
+public class MainWindow extends JFrame{
+	
+	public Toolkit toolKit = Toolkit.getDefaultToolkit();
 
 	public MainWindow(){
+		initFrame();
+		initContainer(this);
+	}
+
+	private void initContainer(JFrame fr) {
+		Container contentPane = fr.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(new JLabel("Hello World main"), BorderLayout.CENTER);
+
+		JButton exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		contentPane.add(exitButton, BorderLayout.SOUTH);
+		
 		
 	}
+
+	private void initFrame() {
+		System.setProperty("charva.color", "");
+		setTitle("Book Phones");
+		setBackground(Color.blue);
+		setSize(toolKit.getScreenSize());
+		
+	}
+	
+	
 	
 	
 }
